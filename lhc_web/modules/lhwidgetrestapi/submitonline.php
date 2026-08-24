35cc709247a4d46808c1e5b28a6a1517284427c7
Remigijus Kiminas <remdex@gmail.com>
2026-08-24 12:39:38 +0300

    ✨ Pass invisible message arguments to theme bot trigger execution

--- lhc_web/modules/lhwidgetrestapi/submitonline.php
+++ lhc_web/modules/lhwidgetrestapi/submitonline.php
@@ -435,9 +435,21 @@ if (empty($Errors)) {
         ) {
             $trigger = erLhcoreClassModelGenericBotTrigger::fetch($additionalParams['theme']->bot_configuration_array['trigger_id']);
             $paramsExecution['trigger_id_executed'] = $additionalParams['theme']->bot_configuration_array['trigger_id'];
+
+            if (isset($requestPayload['fields']['trigger_args']) && is_array($requestPayload['fields']['trigger_args'])) {
+                if (!isset($paramsExecution['replace_array'])) {
+                    $paramsExecution['replace_array'] = [];
+                }
+                foreach ($requestPayload['fields']['trigger_args'] as $argKey => $argVal) {
+                    if (preg_match('/^\{message_invisible_([1-9]|10)\}$/', $argKey) && (is_int($argVal) || is_string($argVal))) {
+                        $paramsExecution['replace_array'][$argKey] = $argVal;
+                    }
+                }
+            }
+
             if (is_object($trigger)) {
                 erLhcoreClassGenericBotWorkflow::$setBotFlow = true;
-                erLhcoreClassGenericBotWorkflow::processTrigger($chat, $trigger);
+                erLhcoreClassGenericBotWorkflow::processTrigger($chat, $trigger, false, array('args' => $paramsExecution));
                 erLhcoreClassGenericBotWorkflow::$setBotFlow = false;
                 $triggerEvent = erLhcoreClassModelGenericBotChatEvent::findOne(array('filter' => array('chat_id' => $chat->id)));
             }
